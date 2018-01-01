23a8a48c48b209ccecf4f3d5a5ac92e715d0e2e5
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-01-01 12:09:49 +0100

    fix pylint E231

--- filter.py
+++ filter.py
@@ -22,16 +22,16 @@ if os.path.isfile(ifile):
 
   for line in lines:
     if DEBUG:
-      print("  ",line)
+      print("  ", line)
     # remove any leading or trailing whitespace
     s = line.strip()
     if DEBUG:
-      print("   ",s)
+      print("   ", s)
     # check if there is something left
     if len(s) > 0:
       si = s.split()
       if DEBUG:
-        print("    ",si)
+        print("    ", si)
       if len(si) > 0:
         try:
           socket.inet_aton(si[0])
@@ -48,12 +48,12 @@ if os.path.isfile(ifile):
       # empty lines are replaced by "#"; not to worry. "#" are removed later
       sit = "#"
     if DEBUG:
-      print("     ",sit)
+      print("     ", sit)
     # output the result; excluding "#"
     if sit[0] is not "#":
       site = sit
       if DEBUG:
-        print(">>>> ",site)
+        print(">>>> ", site)
       newlines.append(site)
     if DEBUG:
       print("")
