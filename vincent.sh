02be9e3878477380a1b2d13bf81ee3a551412e12
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-01-27 16:46:14 +0100

    FreeBSD V.I.N.CENT

--- vincent.sh
+++ vincent.sh
@@ -4,16 +4,8 @@
 
 # Find out where we're running from
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# Get our IPv4
-CYGNUS_IP4=$(ip route get 1 | awk '{print $NF;exit}')
-# Get our IPv6
-CYGNUS_IP6=$(ip addr show dev eth0 | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d' | awk ' { if ( length > x ) { x = length; y = $0 } }END{ print y }')
-# Use the defaults from Pi-hole if they exist
-if [[ -f /etc/pihole/setupVars.conf ]]; then
-  CYGNUS_IP4=$(grep "IPV4" /etc/pihole/setupVars.conf | awk -F "=" '{print $2}'| awk -F "/" '{print $1}')
-  CYGNUS_IP6=$(grep "IPV6" /etc/pihole/setupVars.conf | awk -F "=" '{print $2}')
-fi
-
+# shellcheck source=/dev/null
+source "${SCRIPT_DIR}/bin/include"
 
 # USS Cygnus's sources of blocklists are very divers:
 # Sources that supply just a list of hostnames, one per line:
@@ -25,14 +17,10 @@ CYGNUS_URL_LIST="${SCRIPT_DIR}/sources.url.txt"
 # Sources that supply a list of IPs:
 CYGNUS_IPs_LIST="${SCRIPT_DIR}/sources.ip.txt"
 
-# Pi-hole keeps it's own hosts list. We will absorb this into ours
-PIHOLE_GRAVITY_LIST="/etc/pihole/gravity.list"
-# to be added: dnsmasq native methods
-
 # USS Cygnus allows you're own blacklist and will add it if it exists.
-CYGNUS_LOCALBLACK_LIST="${HOME}/.config/cygnus/black.list"
+CYGNUS_LOCALBLACK_LIST="${CYGNUS_CONFIG_DIR}/black.list"
 # USS Cygnus allows you're personal whitelist
-CYGNUS_LOCALWHITE_LIST="${HOME}/.config/cygnus/white.list"
+CYGNUS_LOCALWHITE_LIST="${CYGNUS_CONFIG_DIR}/white.list"
 # USS Cygnus also comes with it's own whitelist
 CYGNUS_WHITE_LIST="${SCRIPT_DIR}/white.list"
 
@@ -54,12 +42,8 @@ wget --timeout=20 -qnv -i "${CYGNUS_URL_LIST}" -O - |\
   cut -c8- |\
   awk -F/ '{print $1}' >> "${TMP_FILE}"
 
-if [[ -f "${PIHOLE_GRAVITY_LIST}" ]]; then
-  echo "Absorbing Pi-hole's list..."
-  awk '{print $2}' "${PIHOLE_GRAVITY_LIST}" >> "${TMP_FILE}"
-fi
 
-# At this point we have a file that consolidates our own sources and the Pi-hole gravity.list
+# At this point we have a file that consolidates all our sources.
 # Now, we add the user's Black List
 if [[ -f "${CYGNUS_LOCALBLACK_LIST}" ]]; then
   echo "Consolidating the local BLACKLIST..."
@@ -88,7 +72,7 @@ mv "${TMP_FILE}" "${CYGNUS_OUTPUT}"
 #echo "Adding 'IP' lines..."
 #wget --timeout=20 -qnv -i ${CYGNUS_IPs_LIST} -O  - | sed -e '/\s*#.*$/d' -e '/^\s*$/d' >> ${TMP_FILE}
 
-# Next we must remove the white-listed sites.
+# Next we will remove the white-listed sites.
 # use tempfiles for fgrep, because pipes can't be used due to conditional executions
 TMP_FILE=$(mktemp /tmp/cygnus.XXXXXX)
 if [[ -f "${CYGNUS_WHITE_LIST}" ]]; then
@@ -107,7 +91,7 @@ fi
 # Finally the list must be converted to a hosts list: prepending IP4 and IP6 to each line.
 echo "Converting list to hosts format..."
 TMP_FILE=$(mktemp /tmp/cygnus.XXXXXX)
-awk -v ip4="${CYGNUS_IP4}" -v ip6="${CYGNUS_IP6}" '{print ip4 "  " $0;print ip6 "  " $0;}' "${CYGNUS_OUTPUT}" > "${TMP_FILE}"
+awk -v ip4="${BLACKHOLE_IP4}" -v ip6="${BLACKHOLE_IP6}" '{print ip4 "  " $0;print ip6 "  " $0;}' "${CYGNUS_OUTPUT}" > "${TMP_FILE}"
 mv "${TMP_FILE}" "${CYGNUS_OUTPUT}"
 
 echo ""
@@ -119,13 +103,14 @@ echo ""
 echo ""
 
 echo "Moving list into place..."
-# temporary move; while transitioning from Pi-hole to pure dnsmasq
-sudo mv "${CYGNUS_OUTPUT}" "${PIHOLE_GRAVITY_LIST}"
-sudo chmod +r "${PIHOLE_GRAVITY_LIST}"
-sudo chown root:root "${PIHOLE_GRAVITY_LIST}"
+sudo mv "${BLACKLIST}" "${BLACKLIST}.bak"
+sudo mv "${CYGNUS_OUTPUT}" "${BLACKLIST}"
 
-echo "$(wc -l ${PIHOLE_GRAVITY_LIST} | awk '{print $1 "/ 2"}' | bc) domains will be blocked"
+sudo chmod 744 "${BLACKLIST}"
+sudo chown root:wheel "${BLACKLIST}"
+
+echo "$(wc -l ${BLACKLIST} | awk '{print $1 "/ 2"}' | bc) domains will be blocked"
 
 echo "Restarting DNS to activitate the new list..."
-sudo systemctl restart dnsmasq.service
-sudo systemctl restart pihole-FTL.service
+
+sudo pluginctl dns
