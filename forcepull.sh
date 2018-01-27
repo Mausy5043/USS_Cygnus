ff46716c813b17135e314845a2d36eb8bf3cda94
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2018-01-27 08:53:28 +0100

    Create forcepull.sh

---
+++ forcepull.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+
+if [[ ! -f "${HOME}/.cygnus.branch" ]]; then
+  echo "${HOME}/.cygnus.branch not set!"
+  exit 1
+else
+  BRANCH=$( cat "${HOME}/.cygnus.branch" )
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source bin/include
+
+pushd "${DIR}" || exit 1
+  # shellcheck disable=SC1091
+  git fetch origin
+  # Check which files have changed
+  DIFFLIST=$(git --no-pager diff --name-only "$BRANCH..origin/$BRANCH")
+  git pull
+  git fetch origin
+  git checkout "$BRANCH"
+  git reset --hard "origin/$BRANCH" && git clean -f -d
+popd
